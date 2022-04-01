b1380e0d45a9388da092846705a7625963dc8beb
raj <rajjadhav1043@gmail.com>
2022-04-01 13:26:13 +0530

    letsdoit1 commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@
 
 import 'package:clickncook/screens/home_screen.dart';
 import 'package:clickncook/screens/login_screen.dart';
+import 'package:clickncook/services/auth_services.dart';
+import 'package:clickncook/wraper.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:clickncook/utils/routes.dart';
+import 'package:provider/provider.dart';
+// import 'dart:html';
 
-void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -20,23 +26,30 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        Provider<AuthServices>(
+          create: (_) => AuthServices(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // initialRoute: MyRoutes.searchBarroute,
+        routes: {
+          "/": (context) => new Wraper(),
+          "/home": (context) => new HomePage(
+                searchtext: null,
+              ),
+          MyRoutes.homepageroute: (context) => HomePage(
+                searchtext: null,
+              ),
+          // MyRoutes.loginRoute: (context) => LoginPage(),
+        },
       ),
-      // initialRoute: MyRoutes.searchBarroute,
-      routes: {
-        "/": (context) => new LoginScreen(),
-        "/home": (context) => new HomePage(
-              searchtext: null,
-            ),
-        MyRoutes.homepageroute: (context) => HomePage(
-              searchtext: null,
-            ),
-        // MyRoutes.loginRoute: (context) => LoginPage(),
-      },
     );
   }
 }
