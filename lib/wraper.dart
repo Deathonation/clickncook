9bc6593ff71d5ed515088a36f75e1a8136ba10bd
raj <rajjadhav1043@gmail.com>
2022-04-01 13:26:34 +0530

    yeah

---
+++ lib/wraper.dart
@@ -0,0 +1,31 @@
+import 'package:clickncook/models/user_models.dart';
+import 'package:clickncook/screens/home_screen.dart';
+import 'package:clickncook/screens/login_screen.dart';
+import 'package:clickncook/services/auth_services.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Wraper extends StatelessWidget {
+  const Wraper({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final authService = Provider.of<AuthServices>(context);
+    return StreamBuilder<User>(
+        stream: authService.user,
+        builder: (_, AsyncSnapshot<User> snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            final User user = snapshot.data;
+            return user == null
+                ? LoginScreen()
+                : HomePage(
+                    searchtext: null,
+                  );
+          } else {
+            return Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+        });
+  }
+}
