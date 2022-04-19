aac15f3b85418bebd20cb4e2d28c0092ef112340
raj <rajjadhav1043@gmail.com>
2022-04-19 19:06:17 +0530

    image upload to firestore is done

--- lib/pickImage.dart
+++ lib/pickImage.dart
@@ -1,7 +1,12 @@
+import 'package:clickncook/services/auth_services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:clickncook/screens/home_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 import 'package:tflite/tflite.dart';
 
@@ -17,6 +22,7 @@ class _ImageSearchState extends State<ImageSearch> {
   List _outputs;
   bool _loading = false;
   String passOutput;
+  String url;
 
   @override
   void initState() {
@@ -74,6 +80,23 @@ class _ImageSearchState extends State<ImageSearch> {
     });
   }
 
+  // Future classifyImage(File image) async {
+  //   int startTime = new DateTime.now().millisecondsSinceEpoch;
+  //   var recognitions = await Tflite.runModelOnImage(
+  //     path: image.path,
+  //     numResults: 6,
+  //     threshold: 0.05,
+  //     imageMean: 127.5,
+  //     imageStd: 127.5,
+  //   );
+  //   setState(() {
+  //     _loading = false;
+  //     _outputs = recognitions;
+  //   });
+  //   int endTime = new DateTime.now().millisecondsSinceEpoch;
+  //   print("Inference took ${endTime - startTime}ms");
+  // }
+
   @override
   void dispose() {
     Tflite.close();
@@ -83,17 +106,61 @@ class _ImageSearchState extends State<ImageSearch> {
   final ImagePicker imgPicker = new ImagePicker();
   Future getImage() async {
     // ignore: deprecated_member_use
+    final _auth = FirebaseAuth.instance;
+    String userEmail = _auth.currentUser.email;
+
     var image = await imgPicker.getImage(source: ImageSource.gallery);
-    if (image == null) return null;
+    if (image == null) {
+      print(
+          "null image BAKA-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+      return null;
+    }
     setState(() {
       _loading = true;
       _image = File(image.path);
     });
     classifyImage(_image);
+    print(
+        "after classify BAKA-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+    final ref = FirebaseStorage.instance
+        .ref("myimages")
+        .child("userImages")
+        .child(userEmail);
+
+    await ref.putFile(_image);
+    url = await ref.getDownloadURL();
+    print(
+        "after ref BAKA-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+    Map<String, dynamic> demodata = {"images": url};
+
+    CollectionReference collectionReference =
+        FirebaseFirestore.instance.collection(userEmail);
+    print(
+        "after CR BAKA-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+    collectionReference
+        .doc("imagesClick")
+        .collection("ImagesClick")
+        .add(demodata);
+
+    print(
+        "after all BAKA-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+    // collectionReference.snapshots().listen((event) {
+    //   setState(() {
+    //     final data = event.docs[0].data();
+    //     print(data);
+    //   });
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
+    // void getUserEmail() async {
+    //   User user = await _auth.currentUser.email;
+    // }
+
     return SafeArea(
       child: Scaffold(
           body: Container(
