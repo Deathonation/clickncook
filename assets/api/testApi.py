82b9a4804844d7cd20adf75a656d4f085e7476c9
raj <rajjadhav1043@gmail.com>
2022-04-23 11:37:29 +0530

    Finally Some Proper Working

--- assets/api/testApi.py
+++ assets/api/testApi.py
@@ -1,18 +1,10 @@
 from __future__ import division, print_function
-import json
 from flask import Flask, jsonify
 from flask_restful import Resource, Api, reqparse
 
-import json
-import sys
 import os
-import glob
-import matplotlib
-import pandas as pd
-import re
 from matplotlib.pyplot import axis
 import numpy as np
-import requests
 import tensorflow as tf
 import keras
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
@@ -22,12 +14,11 @@ import urllib.request
 
 # load  model
 MODEL_PATH = r'D:\RAJ\DMCE_sem8\Major Project\trial1\clickncook\assets\mobilenet1\mobilenetv2Model.h5'
-# MODEL_PATH = "https://firebasestorage.googleapis.com/v0/b/cncauthentication.appspot.com/o/Model%2Fclickncook%2Fmobilenetv2Model.h5?alt=media&token=869eda68-63d6-4a94-9b4d-138e1138c5b5"
-model = load_model(MODEL_PATH)
 labelsPath = r"D:\RAJ\DMCE_sem8\Major Project\trial1\clickncook\assets\mobilenet1\labels.txt"
+# MODEL_PATH = "https://firebasestorage.googleapis.com/v0/b/cncauthentication.appspot.com/o/Model%2Fclickncook%2Fmobilenetv2Model.h5?alt=media&token=869eda68-63d6-4a94-9b4d-138e1138c5b5"
 # labelsPath = "https://firebasestorage.googleapis.com/v0/b/cncauthentication.appspot.com/o/Model%2Fclickncook%2Flabels.txt?alt=media&token=5b3c3035-ad58-4490-8d85-18cbfb52c99f"
-
 # labels = list(urllib.request.urlopen(labelsPath).read().splitlines())
+model = load_model(MODEL_PATH)
 labels = open(labelsPath).read().splitlines()
 print(labels)
 
@@ -38,7 +29,6 @@ parser = reqparse.RequestParser()
 
 
 class ModelApi(Resource):
-    # _img = r"D:\RAJ\DMCE_sem8\Major Project\modelTraining\testing\pavbhaji.jpg"
     def download_image(img_link):
         img_link = str(img_link)
         download_cmd = 'wget --no-check-certificate -O food.jpg '+img_link
@@ -59,7 +49,6 @@ class ModelApi(Resource):
         return jsonify(labels[index])
 
     def prepare_image_and_predict(img_name):
-        # imgpath = r"D:\RAJ\DMCE_sem8\Major Project\modelTraining\testing\dhokla.jpg"
         img = image.load_img(img_name, target_size=(224, 224))
         img_array = image.img_to_array(img)
         img_array_expanded_dims = np.expand_dims(img_array, axis=0)
@@ -69,13 +58,6 @@ class ModelApi(Resource):
         return mobile
 
 
-        # def prepare_image(img):
-    #     # img = image.load_img(file, target_size=(224, 224))
-    #     img_array = image.img_to_array(img)
-    #     img_array_expanded_dims = np.expand_dims(img_array, axis=0)
-    #     mobile = keras.applications.mobilenet_v2.preprocess_input(
-    #         img_array_expanded_dims)
-    #     return mobile
 api.add_resource(ModelApi, '/modelapi/')
 
 
--- lib/pickImage.dart
+++ lib/pickImage.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
@@ -19,10 +18,10 @@ class ImageSearch extends StatefulWidget {
 
 class _ImageSearchState extends State<ImageSearch> {
   File _image;
-  List _outputs;
+  String prediction;
   bool _loading = false;
-  String passOutput;
   String url;
+  var image;
 
   @override
   void initState() {
@@ -37,65 +36,52 @@ class _ImageSearchState extends State<ImageSearch> {
     super.dispose();
   }
 
-  final ImagePicker imgPicker = new ImagePicker();
-  QuerySnapshot snapshot;
-  Future<String> getImage() async {
-    // ignore: deprecated_member_use
-
-    final _auth = FirebaseAuth.instance;
-    String userEmail = _auth.currentUser.email;
-
-    var image = await imgPicker.getImage(source: ImageSource.gallery);
-    if (image == null) {
-      return null;
-    }
-    _image = File(image.path);
-
-    var datetime = DateTime.now();
-    final ref = FirebaseStorage.instance
-        .ref("myimages")
-        .child(userEmail)
-        .child("$datetime");
-
-    await ref.putFile(_image);
-    url = await ref.getDownloadURL();
-    print(url);
-
-    final response = await http.post(
-        Uri.parse("https://ed8d-203-194-99-28.in.ngrok.io/modelapi/"),
-        body: {"img_link": url});
-    print(response.body);
-    final prediction = response.body;
-    Map<String, dynamic> demodata = {prediction.replaceAll('"', ''): url};
-    CollectionReference collectionReference =
-        FirebaseFirestore.instance.collection(userEmail);
-
-    collectionReference
-        .doc("imagesClick")
-        .collection("ImagesClick")
-        .add(demodata);
-
-    // final data = await FirebaseFirestore.instance
-    //     .collection(userEmail)
-    //     .doc("imagesClick")
-    //     .collection("ImagesClick")
-    //     .get();
-
-    // print(data.docs.length);
-    // print(data.docs[data.docs.length - 1].data());
-    // final imgLinkJson = data.docs[data.docs.length - 1].data();
-    // // final imgLink = imgLinkJson["images"];
-    // print(imgLinkJson);
-
-    // final cache = DefaultCacheManager();
-    // await cache.emptyCache();
-    setState(() {});
-    return prediction;
-  }
-
   @override
   Widget build(BuildContext context) {
-    Future<String> prediction;
+    final ImagePicker imgPicker = new ImagePicker();
+    Future<String> getFoodImage(int i) async {
+      // ignore: deprecated_member_use
+
+      final _auth = FirebaseAuth.instance;
+      String userEmail = _auth.currentUser.email;
+      if (i == 0) {
+        image = await imgPicker.getImage(source: ImageSource.camera);
+      } else if (i == 1) {
+        image = await imgPicker.getImage(source: ImageSource.gallery);
+      }
+
+      if (image == null) {
+        return null;
+      }
+      _image = File(image.path);
+
+      var datetime = DateTime.now();
+      final ref = FirebaseStorage.instance
+          .ref("myimages")
+          .child(userEmail)
+          .child("$datetime");
+
+      await ref.putFile(_image);
+      url = await ref.getDownloadURL();
+      print(url);
+
+      final response = await http.post(
+          Uri.parse("https://ed8d-203-194-99-28.in.ngrok.io/modelapi/"),
+          body: {"img_link": url});
+      print(response.body);
+      prediction = response.body;
+      Map<String, dynamic> demodata = {prediction.replaceAll('"', ''): url};
+      CollectionReference collectionReference =
+          FirebaseFirestore.instance.collection(userEmail);
+
+      collectionReference
+          .doc("imagesClick")
+          .collection("ImagesClick")
+          .add(demodata);
+      setState(() {});
+      return prediction;
+    }
+
     return SafeArea(
       child: Scaffold(
           body: Container(
@@ -119,41 +105,40 @@ class _ImageSearchState extends State<ImageSearch> {
                                 ? Container(
                                     child: Text("Error no image"),
                                   )
-                                : Container(
-                                    height: 300,
-                                    width: 300,
-                                    child: Column(
-                                      children: [
-                                        Image.file(
+                                : Column(
+                                    children: [
+                                      Container(
+                                        width: 200,
+                                        height: 300,
+                                        child: Image.file(
                                           _image,
                                         ),
-                                        SizedBox(
-                                          height: 10,
-                                        ),
-                                        Text(prediction.toString())
-                                      ],
-                                    ),
-                                  ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            _image == null
-                                ? Container()
-                                : _outputs != null
-                                    ? Text(
-                                        _outputs[0]['label'],
-                                        style: TextStyle(
-                                            color: Colors.black, fontSize: 20),
-                                      )
-                                    : Container(
-                                        child: Text(""),
                                       ),
-                            SizedBox(
-                              height: MediaQuery.of(context).size.height * 0.01,
-                            )
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      _image == null
+                                          ? Container()
+                                          : prediction != null
+                                              ? Text(
+                                                  prediction.replaceAll(
+                                                      '"', ""),
+                                                  style: TextStyle(
+                                                      color: Colors.black,
+                                                      fontSize: 20),
+                                                )
+                                              : Container(
+                                                  child: Text(""),
+                                                ),
+                                      SizedBox(
+                                        height:
+                                            MediaQuery.of(context).size.height *
+                                                0.01,
+                                      )
+                                    ],
+                                  ),
                           ],
-                        ),
-                      )
+                        ))
               ],
             ),
           ),
@@ -165,11 +150,24 @@ class _ImageSearchState extends State<ImageSearch> {
                   heroTag: "btn1",
                   onPressed: () {
                     setState(() {
-                      prediction = getImage();
+                      getFoodImage(0);
                     });
                   },
                   backgroundColor: Colors.grey,
                   child: Icon(Icons.add_a_photo),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                FloatingActionButton(
+                  heroTag: "btn3",
+                  onPressed: () {
+                    setState(() {
+                      getFoodImage(1);
+                    });
+                  },
+                  backgroundColor: Colors.grey,
+                  child: Icon(Icons.photo_album),
                 ),
                 SizedBox(
                   width: 10,
@@ -186,7 +184,7 @@ class _ImageSearchState extends State<ImageSearch> {
                             context,
                             MaterialPageRoute(
                                 builder: (context) => new HomePage(
-                                      searchtext: passOutput,
+                                      searchtext: prediction.toString(),
                                     )));
                       }),
                 )
@@ -196,9 +194,3 @@ class _ImageSearchState extends State<ImageSearch> {
     );
   }
 }
-
-// FloatingActionButton(
-//           tooltip: "pick Image",
-//           onPressed: getImage,
-//           backgroundColor: Colors.grey,
-//           child: Icon(Icons.add_a_photo),
